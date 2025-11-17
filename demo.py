8973ea9567a26bcdfe42acef3e8edfa9a7bba47d
nkfreeman <nkfreeman@cba.ua.edu>
2025-11-17 14:56:50 -0600

    update data directory structure

--- demo.py
+++ demo.py
@@ -8,18 +8,41 @@ app = marimo.App(width="medium")
 def _():
     import marimo as mo
 
+    import pathlib
+    import socket
+
     import matplotlib.pyplot as plt
     import polars as pl
     import seaborn as sns
 
     sns.set_style('whitegrid')
-    return (pl,)
+    return pathlib, pl, socket
 
 
 @app.cell
-def _(pl):
-    data = pl.read_parquet('data/ad_data.parquet')
-    data.group_by([
+def _(pathlib, socket):
+    dropbox_data_directory_mapper = {
+        'nkf-precision-7865': pathlib.Path('/home/nick/Dropbox/git-demo-data/'),
+    }
+
+    current_hostname = socket.gethostname()
+    if current_hostname in dropbox_data_directory_mapper:
+        dropbox_data_directory = dropbox_data_directory_mapper.get(current_hostname)
+    else:
+        print(f' - Host not found in mapper!!!')
+    return (dropbox_data_directory,)
+
+
+@app.cell
+def _(dropbox_data_directory, pathlib, pl):
+    data_filepath = pathlib.Path(
+        dropbox_data_directory,
+        'ad_data.parquet',
+    )
+
+    data = pl.read_parquet(
+        data_filepath
+    ).group_by([
         'location_state',
         'post_date',
     ]).agg(
@@ -27,6 +50,8 @@ def _(pl):
     ).with_columns(
         location_state = pl.col('location_state').replace({'Rhode-Island': 'Rhode Island'})
     )
+
+    data
     return
 
 
